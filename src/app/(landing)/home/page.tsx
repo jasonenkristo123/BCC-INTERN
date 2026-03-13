0c990b20895d3da34faf7c6d625c09d30a824254
jasonenkristo123 <jasonenkristo@gmail.com>
2026-03-13 22:36:40 +0700

    refactor: refactoring folder features also refactor husky pre-commit (adding npm run build for automate build

---
+++ src/app/(landing)/home/page.tsx
@@ -0,0 +1,10 @@
+
+import LandingContainer from '@/features/landing/containers/landing-container'
+
+export default function LandingPage() {
+  return (
+    <main>
+      <LandingContainer />
+    </main>
+  )
+}
---
+++ src/features/landing/data/landing-data.ts
@@ -0,0 +1,56 @@
+import { TeamMember } from "../hooks/use-team-carousel";
+
+
+export const TeamData: TeamMember[] = [
+    {
+        role: 'Product Manager',
+        name: 'Sthefany\nSheanmoulia',
+        imageSrc: '/assets/dedy.webp',
+    },
+    {
+        role: 'UI/UX Designer',
+        name: 'Shananda\nPutri Aisyah',
+        imageSrc: '/assets/dedy.webp',
+    },
+    {
+        role: 'Front End',
+        name: 'Jason\nEnkristo',
+        imageSrc: '/assets/dedy.webp',
+    },
+    {
+        role: 'Back End',
+        name: 'Rizkya Dwi\nAulya Fasya',
+        imageSrc: '/assets/dedy.webp',
+    },
+]
+
+export const features = [
+    {
+        label: 'Estimasi Masa Simpan',
+        description: 'Ketahui berapa lama bahan makanan bisa disimpan.',
+        imageSrc: '/assets/frame1.webp',
+        imageAlt: 'Ilustrasi kulkas dengan bahan makanan',
+        contentImg: '/assets/kulkas.webp',
+    },
+    {
+        label: 'Reminder Sebelum Basi',
+        description: 'Jangan sampai bahan makanan basi karena lupa.',
+        imageSrc: '/assets/frame2.webp',
+        imageAlt: 'Ilustrasi sayuran segar',
+        contentImg: '/assets/buah.webp',
+    },
+    {
+        label: 'Food Waste Cost Estimator',
+        description: 'Pernah sadar berapa uang yang hilang karena makanan dibuang?',
+        imageSrc: '/assets/frame3.webp',
+        imageAlt: 'Ilustrasi makanan yang terbuang',
+        contentImg: '/assets/buahbusuk.webp',
+    },
+]
+
+export const faktaDataInfo = [
+    { title: '931jt', description: 'Ton Makanan Terbuang (UNEP 2021)' },
+    { title: '23-48 juta ton', description: 'food waste dihasilkan di Indonesia setiap tahunnya - Bappenas' },
+    { title: '61%', description: 'food waste berasal dari rumah makan' },
+    { title: '931jt', description: 'Ton Makanan Terbuang (UNEP 2021)' },
+]
